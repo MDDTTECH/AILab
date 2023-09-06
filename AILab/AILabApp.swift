ad1ee2d00dddc89e378f120b25efdb4c087f44e1
Aleksunder Volkov <t.me/aleksundervolkov>
2023-09-07 00:58:42 +0500

    Initial Commit

---
+++ AILab/AILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AILabApp.swift
+//  AILab
+//
+//  Created by Aleksunder Volkov on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
